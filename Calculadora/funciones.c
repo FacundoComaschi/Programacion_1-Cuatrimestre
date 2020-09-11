7ed8e6bf62fa0d6fc8fb18f67bf2089d82c79a55
Facundo Comaschi <facucomaschi@gmail.com>
2020-09-11 13:04:23 -0300

    Modificacion de los comentarios

--- Calculadora/funciones.c
+++ Calculadora/funciones.c
@@ -1,12 +1,6 @@
 #include <stdio.h>
 #include "funciones.h"
 
-/** \brief Muestra los calculos en pantalla a realizar con los datos ingresados.
-*   \param float X Primer numero ingresado por el usuario.
-*   \param float Y Segundo numero ingresado por el usuario.
-*/
-
-
 void mostrar (float x, float y)
 {
     printf("Calcular la suma de %.1f + %.1f \n",x,y);
@@ -16,21 +10,12 @@ void mostrar (float x, float y)
     printf("Calcular la factorizacion de %.1f y de %.1f \n",x,y);
 }
 
-/** \brief Funcion sumar, realiza la suma y pasa el resultado cuando la funcion es llamada.
-*   \param float a Primer numero ingresado por el usuario.
-*   \param float b Segundo numero ingresado por el usuario.
-*/
-
 void sumar(float a, float b)
 {
 
 printf("El resultado de la suma de %.1f + %.1f es : %.1f \n" ,a,b, a + b);
 
 }
-/** \brief Funcion restar, realiza la resta y pasa el resultado cuando la funcion es llamada.
-*   \param float a Primer numero ingresado por el usuario.
-*   \param float b Segundo numero ingresado por el usuario.
-*/
 
 void restar(float a, float b)
 {
@@ -38,10 +23,6 @@ void restar(float a, float b)
 printf("El resultado de la resta de %.1f - %.1f es : %.1f \n" ,a,b, a - b);
 
 }
-/** \brief Funcion multiplicar, realiza la multiplicacion y pasa el resultado cuando la funcion es llamada.
-*   \param float a Primer numero ingresado por el usuario.
-*   \param float b Segundo numero ingresado por el usuario.
-*/
 
 void multiplicar(float a, float b)
 {
@@ -49,10 +30,6 @@ void multiplicar(float a, float b)
 printf("El resultado de la multiplicacion de %.2f * %.2f es : %.2f \n" ,a,b, a * b);
 
 }
-/** \brief Funcion dividir, realiza la division y pasa el resultado cuando la funcion es llamada.
-*   \param float a Primer numero ingresado por el usuario.
-*   \param float b Segundo numero ingresado por el usuario.
-*/
 
 void dividir(float a, float b)
 {
@@ -60,12 +37,6 @@ void dividir(float a, float b)
 printf("El resultado de la division de %.2f / %.2f es : %.2f \n" ,a,b, a / b);
 
 }
-/** \brief Funcion factorial, realiza la factorizacion de ambos datos ingresados y pasa el resultado cuando la funcion es llamada.
-*   \param int a Primer numero ingresado por el usuario.
-*   \param int b Segundo numero ingresado por el usuario.
-*   \param int numeroFactorialA toma el valor de 1 para luego realizar la multiplicacion y se guarda el resultado.
-*   \param int numeroFactorialB toma el valor de 1 para luego realizar la multiplicaicon y se guarda el resultado.
-*/
 
 void factorial(int a, int b)
 {
--- Calculadora/funciones.h
+++ Calculadora/funciones.h
@@ -1,8 +1,39 @@
 #include <stdio.h>
 
+/** \brief Funcion sumar, realiza la suma y pasa el resultado cuando la funcion es llamada.
+*   \param float a Primer numero ingresado por el usuario.
+*   \param float b Segundo numero ingresado por el usuario.
+*/
 void sumar(float a, float b);
+
+/** \brief Funcion restar, realiza la resta y pasa el resultado cuando la funcion es llamada.
+*   \param float a Primer numero ingresado por el usuario.
+*   \param float b Segundo numero ingresado por el usuario.
+*/
 void restar(float a, float b);
+
+/** \brief Funcion multiplicar, realiza la multiplicacion y pasa el resultado cuando la funcion es llamada.
+*   \param float a Primer numero ingresado por el usuario.
+*   \param float b Segundo numero ingresado por el usuario.
+*/
 void multiplicar(float a, float b);
+
+/** \brief Funcion dividir, realiza la division y pasa el resultado cuando la funcion es llamada.
+*   \param float a Primer numero ingresado por el usuario.
+*   \param float b Segundo numero ingresado por el usuario.
+*/
 void dividir(float a, float b);
+
+/** \brief Funcion factorial, realiza la factorizacion de ambos datos ingresados y pasa el resultado cuando la funcion es llamada.
+*   \param int a Primer numero ingresado por el usuario.
+*   \param int b Segundo numero ingresado por el usuario.
+*   \param int numeroFactorialA toma el valor de 1 para luego realizar la multiplicacion y se guarda el resultado.
+*   \param int numeroFactorialB toma el valor de 1 para luego realizar la multiplicaicon y se guarda el resultado.
+*/
 void factorial(int a, int b);
+
+/** \brief Muestra los calculos en pantalla a realizar con los datos ingresados.
+*   \param float X Primer numero ingresado por el usuario.
+*   \param float Y Segundo numero ingresado por el usuario.
+*/
 void mostrar(float x, float y);
